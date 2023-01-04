7fd8fcb98b25b2da8a8eab7ff1fe5701a607e538
Emirhan Üstün <emirhan7k@outlook.com>
2023-01-04 20:44:27 +0300

    Firt Commit

---
+++ src/test/java/tr/edu/medipol/yoa/metin/MetinIslemleri2Test.java
@@ -0,0 +1,21 @@
+package tr.edu.medipol.yoa.metin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MetinIslemleri2Test {
+
+	@Test
+	public void testBosluklariTemizle() {
+		// GIVE - On Hazirliklar
+		String orjinalMetin = "Uc tas hos     ho  saf  . . .";
+		
+		// WHEN - Asil Testi Gerceklestir
+		String sonuc = MetinIslemleri2.bosluklariTemizle(orjinalMetin);
+		
+		// THEN - Kontroller
+		assertEquals("Uctashoshosaf...", sonuc);
+	}
+
+}
